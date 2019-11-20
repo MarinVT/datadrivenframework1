969f940e468bd2ea620a59393987a85e0be63b48
marin <marinvalentinov13@gmail.com>
2019-11-20 15:32:26 +0200

    test 1231

--- src/main/java/com/datadriver/framework/utils/DateUtils.java
+++ src/main/java/com/datadriver/framework/utils/DateUtils.java
@@ -3,7 +3,7 @@ package com.datadriver.framework.utils;
 import java.util.Date;
 
 public class DateUtils {
-	
+	//Test git + github 2222
 	public static String getTimeStamp() {
 		Date date = new Date();
 		return date.toString().replaceAll(":", "_").replaceAll(" ", "_");
